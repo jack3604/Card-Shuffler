ff55982f9a10cfb59247484d6652ffdc8d0e5986
akeenan360 <akeenan360@gmail.com>
2024-10-30 01:38:18 -0400

    added javadoc comments to Deck and Card classes

--- Card.java
+++ Card.java
@@ -1,24 +1,49 @@
-import java.util.Map;
+/**
+ * Card class for module 6.
+ * This Card class is used by the Deck class for each card to be an object.
+ */
 
+import java.util.Map;
 public class Card {
     private final String Suit;
     private final int Rank;
 
+    /**
+     * Default constructor for a Card object.
+     * assigns String value of "none" to the Suit and 0 to the Rank.
+     */
     Card() {
         this.Suit = "none";
         this.Rank = 0;
     }
 
+    /**
+     * Parameterized constructor for a Card object.
+     *
+     * @param suit  The suit of the card as a String. The input is formated to be a capital letter followed by lowercase.
+     * @param rank  The rank of the card as an integer.
+     */
     Card(String suit, int rank) {
         this.Suit = suit.substring(0,1).toUpperCase() + suit.substring(1).toLowerCase();
         this.Rank = rank;
     }
 
+    /**
+     * Gets the Suit property of the card.
+     * @return The Suit of the Card as a string.
+     */
     public String GetSuit() { return Suit; }
 
+    /**
+     * Gets the Rank property of the card.
+     * @return The Rank of the card as an integer.
+     */
     public int GetRank() { return Rank; }
 
-    // returns characters
+    /**
+     * Gets a short-form summary of the card.
+     * @return A summary of the card as a String.
+     */
     public String ToString() {
         Map<Integer, String> faceMap = Map.of(1, "J", 2, "Q", 3, "K");
         String suit = Suit.substring(0,1).toUpperCase();
@@ -31,7 +56,11 @@ public class Card {
         return Rank + " of " + suit;
     }
 
-    // overload for word form
+    /**
+     * Gets a long-form summary of the card.
+     * @param longForm boolean if true return long form
+     * @return A long-form summary of the card as a String.
+     */
     public String ToString(boolean longForm) {
         if (longForm) {
             Map<Integer, String> faceMap = Map.of(1, "Jack", 2, "Queen", 3, "King");
@@ -46,6 +75,11 @@ public class Card {
         } else { return this.ToString(); }
     }
 
+    /**
+     * Compares the card calling the method to the card passed.
+     * @param other The other Card object to compare this card to.
+     * @return A short String describing how this card compares to the other card.
+     */
     public String CompareTo(Card other) {
         if (this.Rank > other.Rank) {
             return this.ToString() + " is bigger than " + other.ToString();
--- Deck.java
+++ Deck.java
@@ -1,13 +1,23 @@
+/**
+ * Deck class for module 6.
+ * This Deck class is used to demo shuffling cards using arrays and loops.
+ */
+
 import java.util.Arrays;
 import java.util.Random;
-
 public class Deck {
     protected Card[] Deck;
 
+    /**
+     * Default constructor for Deck object. Creates an empty Card array of length 52.
+     */
     Deck() {
         this.Deck = new Card[52];
     }
 
+    /**
+     * Fills the Deck object with a standard set of cards without jokers.
+     */
     public void Fill() {
         int count = 0;
         for (int suit = 0; suit < 4; suit++){
@@ -31,6 +41,9 @@ public class Deck {
         }
     }
 
+    /**
+     * Uses a Random() object to randomly shuffle the deck.
+     */
     public void Shuffle() {
         Random rand = new Random();
         Card[] newDeck = new Card[52];
@@ -47,6 +60,10 @@ public class Deck {
         this.Deck = newDeck;
     }
 
+    /**
+     * Uses a Random() object to randomly shuffle the deck.
+     * @param timesToShuffle The amount of times to repeat the shuffle.
+     */
     public void Shuffle(int timesToShuffle) {
         for (int i = 0; i < timesToShuffle; i++) {
             Random rand = new Random();
@@ -65,7 +82,9 @@ public class Deck {
         }
     }
 
-    // classic bridge shuffle interpretation. split in half alternate sides.
+    /**
+     * Uses the Riffle shuffle method of splitting the deck in half and alternating sides.
+     */
     public void RiffleShuffle() {
         Card[] half1, half2 = new Card[26];
         Card[] newDeck = new Card[52];
@@ -83,7 +102,10 @@ public class Deck {
         this.Deck = newDeck;
     }
 
-    // classic bridge shuffle interpretation. split in half alternate sides. additional shuffles.
+    /**
+     * Uses the Riffle shuffle method of splitting the deck in half and alternating sides.
+     * @param timesToShuffle The amount of times to repeat the shuffle.
+     */
     public void RiffleShuffle(int timesToShuffle) {
         for (int i = 0; i < timesToShuffle; i++) {
             Card[] half1, half2 = new Card[26];
@@ -103,6 +125,12 @@ public class Deck {
             }
     }
 
+    /**
+     * Uses the Fisher-Yates method of shuffling the cards. This is used to give a psuedo-random shuffle
+     * while trying to be the most computationally efficient. Starting from one side of the deck, you
+     * take the first card and swap it with a randomly selected card and leave that card as shuffled.
+     * Then move to the next and do the same, while only selecting from the previously un-shuffled cards.
+     */
     public void FisherYatesShuffle() {
         Random rand = new Random();
         int i = Deck.length;
@@ -115,6 +143,13 @@ public class Deck {
         }
     }
 
+    /**
+     * Uses the Fisher-Yates method of shuffling the cards. This is used to give a psuedo-random shuffle
+     * while trying to be the most computationally efficient. Starting from one side of the deck, you
+     * take the first card and swap it with a randomly selected card and leave that card as shuffled.
+     * Then move to the next and do the same, while only selecting from the previously un-shuffled cards.
+     * @param timesToShuffle The amount of times to repeat the shuffle.
+     */
     public void FisherYatesShuffle(int timesToShuffle) {
         for (int j = 0; j < timesToShuffle; j++) {
             Random rand = new Random();
@@ -129,6 +164,10 @@ public class Deck {
         }
     }
 
+    /**
+     * Gets a list of the cards in the deck in order from first to last and separates them on new lines.
+     * @return A String of all the cards in the deck in short-form.
+     */
     public String ToString() {
         String out = "";
 
@@ -139,6 +178,12 @@ public class Deck {
         return out;
     }
 
+    /**
+     *
+     * Gets a list of the cards in the deck in order from first to last and separates them on new lines.
+     * @param longForm boolean if true returns long-form.
+     * @return A String of all the cards in the deck in long-form.
+     */
     public String ToString(boolean longForm) {
         if (longForm) {
             String out = "";
